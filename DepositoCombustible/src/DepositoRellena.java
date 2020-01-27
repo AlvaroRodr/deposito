b1ef072183597e00bf98215596bc3dd891ab82f5
Álvaro Rodríguez <alvaro.rodriguez.martinez98@gmail.com>
2020-01-27 09:56:31 +0100

    Modificacion

---
+++ DepositoCombustible/src/DepositoRellena.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DepositoParam.class, TestDeposito.class })
+public class DepositoRellena {
+
+}
---
+++ DepositoCombustible/src/TestDeposito.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+class TestDeposito {
+
+/*	@Test
+	void testDepositoCombustible() {
+		fail("Not yet implemented");
+	}*/
+
+	@Test
+	void testGetDepositoNivelRSS() {
+		DepositoCombustible dep = new DepositoCombustible(2000,1000);
+		double lev = dep.getDepositoNivel();
+		assertEquals(1000,lev);
+	}
+
+	@Test
+	void testGetDepositoMaxRSS() {
+		DepositoCombustible dep = new DepositoCombustible(2000,1000);
+		double max = dep.getDepositoMax();
+		assertEquals(2000,max);
+	}
+
+	@Test
+	void testEstaVacioRSS() {
+		DepositoCombustible dep = new DepositoCombustible(2000,1000);
+		boolean vac = dep.estaVacio();
+		assertFalse(vac);
+
+	}
+
+	@Test
+	void testEstaLlenoMitadRSS() {
+		DepositoCombustible dep = new DepositoCombustible(2000,1000);
+		double lev = dep.getDepositoNivel();
+		double max = dep.getDepositoMax();
+		assertEquals( max , lev*2 );
+	}
+
+
+
+	@Test
+	void testFill() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
